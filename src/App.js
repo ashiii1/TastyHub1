a316bc0cad453590f015be5160598e8c1628e7c3
ashiii1 <aashishaik526@gmail.com>
2024-05-16 23:37:29 +0530

    routes

--- src/App.js
+++ src/App.js
@@ -6,29 +6,25 @@ import Meal from "./components/Meal";
 import NewsLetter from "./components/NewsLetter";
 import TopNav from "./components/TopNav";
 import TopPicks from "./components/TopPicks";
-import { Route } from 'react-router-dom';
+import {  Routes, Route } from 'react-router-dom';
 // import Switch from "react-router-dom"
 
 function App() {
   return (
     <div className="App">
       <TopNav />
-        <Route exact path="/">
-          <Featured />
-        </Route>
+      <Routes>
+        <Route exact path="/" Component={<Featured/>} />
         {/* <Route path="/delivery" component={Delivery} /> */}
-        <Route path="/toppicks">
-          <TopPicks />
-        </Route>
-        <Route path="/meal">
-          <Meal />
-        </Route>
-        <Route path="/categories">
-          <Categories />
-        </Route>
-        <Route path="/newsletter">
-          <NewsLetter />
-        </Route>
+        <Route path="/toppicks" Component={<TopPicks/>}/>
+        
+        <Route path="/meal" Component={<Meal/>}/>
+          
+        <Route path="/categories" Component={<Categories/>}/>
+         
+        <Route path="/newsletter" Component={<NewsLetter/>}/>
+        
+        </Routes>
       <Footer />
     </div>
   );
